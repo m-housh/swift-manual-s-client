e5ec289da132cd87fdebf1d3b8153c99784cbae0
Michael Housh <michael@mhoush.com>
2026-02-19 15:27:09 -0500

    fix: Add platforms to package.swift.

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
   name: "ManualS",
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v10_15),
+    .tvOS(.v13),
+    .watchOS(.v6),
+  ],
   products: [
     .library(name: "ManualSClient", targets: ["ManualSClient"]),
     .library(name: "ManualSModels", targets: ["ManualSModels"]),
