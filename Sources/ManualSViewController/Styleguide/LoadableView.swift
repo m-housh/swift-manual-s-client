5e30d583a50e4ba278042d91cb55ca08178430d3
Michael Housh <michael@mhoush.com>
2026-03-10 19:59:41 -0400

    feat: Adds loadable view.
    This view calls a route,  showing a placeholder while calling the route.

---
+++ Sources/ManualSViewController/Styleguide/LoadableView.swift
@@ -0,0 +1,35 @@
+import Elementary
+import ElementaryHTMX
+import ManualSRouter
+import SharedStyleguide
+
+struct LoadableView<Placeholder: HTML>: HTML, Sendable where Placeholder: Sendable {
+  private let route: ManualSRoute
+  private let placeholder: @Sendable () -> Placeholder
+
+  init(
+    route: ManualSRoute,
+    @HTMLBuilder placeholder: @escaping @Sendable () -> Placeholder
+  ) {
+    self.route = route
+    self.placeholder = placeholder
+  }
+
+  var body: some HTML {
+    div(
+      .class("relative"),
+      .hx.get(route: route),
+      .hx.target("this"),
+      .hx.trigger(.event(.revealed)),
+      .hx.swap(.innerHTML),
+      .hx.indicator(),
+    ) {
+      div(.class("opacity-50")) {
+        placeholder()
+      }
+      div(.class("absolute top-0 grid place-items-center h-full w-full z-50")) {
+        Indicator()
+      }
+    }
+  }
+}
